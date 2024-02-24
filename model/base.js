765f8c299439f15306f2c0b20e3ca96b34445443
actions-user <action@github.com>
2024-02-24 15:04:25 +0000

    TS Build: 205150f9b736bdd87f04596ed2b32588b31a528d

--- model/base.js
+++ model/base.js
@@ -108,7 +108,7 @@ export class Piece {
             return null;
         if (!(this.name in this.parentSet.pieceData))
             return null;
-        return Path.Image + this.parentSet.pieceData[this.name].image;
+        return Path.Image + "/" + this.parentSet.pieceData[this.name].image;
     }
     rollMainStat() {
         this.mainStat = this.mainStatList.choice().instance();
