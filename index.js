1bc573286139f957ce1cd2a81353ee3eaf04baf5
__stdcall <hanghang0702@hotmail.com>
2024-02-24 19:14:13 +0800

    Truncated history

---
+++ index.js
@@ -0,0 +1,39 @@
+import fs from 'node:fs'
+import './model/init.js'
+
+const files = fs.readdirSync('./plugins/gacha-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+logger.info('-----------------')
+logger.info('小叶插件初始化~')
+
+
+files.forEach((file) => {
+    ret.push(import(`./apps/${file}`))
+})
+
+if (!global.segment) {
+    try {
+        global.segment = (await import('oicq')).segment
+    } catch (err) {
+        global.segment = (await import('icqq')).segment
+    }
+}
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+    let name = files[i].replace('.js', '')
+
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+
+
+export { apps }
