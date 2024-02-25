e914837e5585896c582827d72d3b14a189915353
actions-user <action@github.com>
2024-02-25 08:08:51 +0000

    TS Build: 8d3412d90aac5aad985f474e6252271ff5228705

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import fs from 'node:fs';
-const files = fs.readdirSync('./plugins/gacha-plugin/apps').filter(file => file.endsWith('.js'));
+import { Path } from '#gc';
+const dirs = fs.readdirSync(Path.Resource + '/starrail', { withFileTypes: true })
+    .filter(item => item.isDirectory())
+    .map(item => item.name);
 let ret = [];
+for (let i = 0; i < dirs.length; ++i) {
+    // @ts-ignore
+    logger.info(`[gacha-plugin-SR] loading ${dirs[i]}..`);
+    ret.push(import(`${Path.Resource}/starrail/${dirs[i]}/index.js`));
+}
+ret = await Promise.allSettled(ret);
+for (let i = 0; i < dirs.length; ++i) {
+    if (ret[i].status != 'fulfilled') {
+        // @ts-ignore
+        logger.error(`[gacha-plugin-SR] Failed to load ${dirs[i]}`);
+        // @ts-ignore
+        logger.error(ret[i].reason);
+        continue;
+    }
+}
+ret = [];
+const files = fs.readdirSync('./plugins/gacha-plugin/apps').filter(file => file.endsWith('.js'));
 files.forEach((file) => {
     ret.push(import(`./apps/${file}`));
 });
