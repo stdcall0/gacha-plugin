43c2b22e770a3d0a2897d7c555daea2b3e87b011
actions-user <action@github.com>
2024-02-25 09:59:52 +0000

    TS Build: 8fc3ee755a09ed14e2b7028933683148f35f0f09

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import fs from 'node:fs';
-import { Path } from '#gc';
+import { Path, Logger } from '#gc';
 await (async function loadSRdata() {
     await import('#@/resources/starrail/index.js');
     const dirs = fs.readdirSync(Path.Resource + '/starrail', { withFileTypes: true })
@@ -7,17 +7,14 @@ await (async function loadSRdata() {
         .map(item => item.name);
     let ret = [];
     for (let i = 0; i < dirs.length; ++i) {
-        // @ts-ignore
-        logger.info(`[gacha-plugin-SR] loading ${dirs[i]}..`);
+        Logger.info(`[gacha-plugin-SR] loading ${dirs[i]}..`);
         ret.push(import(`${Path.Resource}/starrail/${dirs[i]}/index.js`));
     }
     ret = await Promise.allSettled(ret);
     for (let i = 0; i < dirs.length; ++i) {
         if (ret[i].status != 'fulfilled') {
-            // @ts-ignore
-            logger.error(`[gacha-plugin-SR] Failed to load ${dirs[i]}`);
-            // @ts-ignore
-            logger.error(ret[i].reason);
+            Logger.error(`[gacha-plugin-SR] Failed to load ${dirs[i]}`);
+            Logger.error(ret[i].reason);
             continue;
         }
     }
@@ -27,25 +24,13 @@ const files = fs.readdirSync('./plugins/gacha-plugin/apps').filter(file => file.
 files.forEach((file) => {
     ret.push(import(`./apps/${file}`));
 });
-if (!global.segment) {
-    try {
-        // @ts-ignore
-        global.segment = (await import('oicq')).segment;
-    }
-    catch (err) {
-        // @ts-ignore
-        global.segment = (await import('icqq')).segment;
-    }
-}
 ret = await Promise.allSettled(ret);
 let apps = {};
 for (let i in files) {
     let name = files[i].replace('.js', '');
     if (ret[i].status != 'fulfilled') {
-        // @ts-ignore
-        logger.error(`载入插件错误：${logger.red(name)}`);
-        // @ts-ignore
-        logger.error(ret[i].reason);
+        Logger.error(`载入插件错误：${Logger.red(name)}`);
+        Logger.error(ret[i].reason);
         continue;
     }
     apps[name] = ret[i].value[Object.keys(ret[i].value)[0]];
