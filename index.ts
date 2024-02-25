8d3412d90aac5aad985f474e6252271ff5228705
__stdcall <hanghang0702@hotmail.com>
2024-02-25 16:08:26 +0800

    Hopefully fix resources loading

--- index.ts
+++ index.ts
@@ -1,8 +1,35 @@
 import fs from 'node:fs'
 
+import { Path } from '#gc';
+
+const dirs = fs.readdirSync(Path.Resource + '/starrail', {withFileTypes: true})
+        .filter(item => item.isDirectory())
+        .map(item => item.name);
+
+let ret = [];
+
+for (let i = 0; i < dirs.length; ++i) {
+    // @ts-ignore
+    logger.info(`[gacha-plugin-SR] loading ${dirs[i]}..`);
+    ret.push(import(`${Path.Resource}/starrail/${dirs[i]}/index.js`));
+}
+
+ret = await Promise.allSettled(ret);
+
+for (let i = 0; i < dirs.length; ++i) {
+    if (ret[i].status != 'fulfilled') {
+        // @ts-ignore
+        logger.error(`[gacha-plugin-SR] Failed to load ${dirs[i]}`);
+        // @ts-ignore
+        logger.error(ret[i].reason)
+        continue
+    }
+}
+
+ret = []
+
 const files = fs.readdirSync('./plugins/gacha-plugin/apps').filter(file => file.endsWith('.js'))
 
-let ret = []
 files.forEach((file) => {
     ret.push(import(`./apps/${file}`))
 })
--- resources/starrail/index.ts
+++ resources/starrail/index.ts
@@ -1,26 +1,9 @@
 import fs from 'fs';
 
-import { Path } from '#gc';
-
 export * from './stat.js';
 export * from './piece.js';
 
 export * from './scorer.js';
-
-const dirs = fs.readdirSync(Path.Resource + '/starrail', {withFileTypes: true})
-        .filter(item => item.isDirectory())
-        .map(item => item.name);
-
-for (let i = 0; i < dirs.length; ++i)
-    try {
-        // @ts-ignore
-        logger.info(`[gacha-plugin-SR] loading ${dirs[i]}..`);
-        await import(`./${dirs[i]}/index.js`);
-    }
-    catch (e) {
-        // @ts-ignore
-        logger.warn(`[gacha-plugin-SR] Failed to load ${dirs[i]}: ${e}`);
-    }
 
 import { Sets as sets } from './set.js';
 import { Domains as domains } from './domain.js';
