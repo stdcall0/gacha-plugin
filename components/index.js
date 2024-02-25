cacd88b5bdc01d397e271bfa45e3c00fb180f4e6
actions-user <action@github.com>
2024-02-25 09:56:47 +0000

    TS Build: 49331d13ec07c88305b76fee220fb5846af23a1c

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ import Lottery from './lottery.js';
 import Common from './common.js';
 import Path from './cpath.js';
 import Plugin from './plugin.js';
+import Logger from './logger.js';
 export * from './displaymode.js';
 export * from './utils.js';
-export { Render, Lottery, Common, Path, Plugin };
+export { Render, Lottery, Common, Path, Plugin, Logger };
