19ef4e740400b857fa3c3611cc6bf789b12ae9bf
actions-user <action@github.com>
2024-04-16 02:16:55 +0000

    TS Build: df4e0a8dbfe8062ad0dcc9cf157e2150d88e805a

--- apps/starrail_gacha.js
+++ apps/starrail_gacha.js
@@ -1,6 +1,8 @@
 import { Plugin } from '#gc';
 import { StarRail } from '#gc.model';
 import { StarRailData } from '#gc.res';
+;
+let rank = {};
 let state = {};
 export class SRGachaPlugin extends Plugin {
     constructor() {
@@ -21,6 +23,10 @@ export class SRGachaPlugin extends Plugin {
                 {
                     reg: '^#星铁单抽.*$',
                     fnc: 'single'
+                },
+                {
+                    reg: '^#星铁抽卡排名.*$',
+                    fnc: 'showrank'
                 }
             ]
         });
@@ -87,25 +93,87 @@ export class SRGachaPlugin extends Plugin {
             msg.push(`共 ${s5.length} 个 5*，${s4.length} 个 4*，已垫 ${this.lastFive(res[res.length - 1])} 抽`);
         }
         await this.reply(msg.join('\n'), true);
-        return gacha;
+        return {
+            gacha,
+            star5: s5.length,
+            up: s5.filter(x => x.item.up).length,
+            total: n
+        };
+    }
+    initRank(key, name) {
+        if (!rank[key])
+            rank[key] = { name: name, star5: 0, up: 0, total: 0 };
+        else
+            rank[key].name = name;
+    }
+    updateRank(key, res) {
+        rank[key].star5 += res.star5;
+        rank[key].up += res.up;
+        rank[key].total += res.total;
     }
     async single() {
         const key = this.e.user_id;
+        this.initRank(key, this.e.sender.nickname);
         let gacha = this.getGacha(key);
-        gacha = await this.n(1, gacha, false);
-        this.saveGacha(key, gacha);
+        let res = await this.n(1, gacha, false);
+        this.saveGacha(key, res.gacha);
+        this.updateRank(key, res);
     }
     async ten() {
         const key = this.e.user_id;
+        this.initRank(key, this.e.sender.nickname);
         let gacha = this.getGacha(key);
-        gacha = await this.n(10, gacha, true);
+        let res = await this.n(1, gacha, false);
         this.saveGacha(key, gacha);
+        this.updateRank(key, res);
     }
     async hundred() {
         const key = this.e.user_id;
+        this.initRank(key, this.e.sender.nickname);
         let gacha = this.getGacha(key);
-        gacha = await this.n(100, gacha, true);
+        let res = await this.n(1, gacha, false);
         this.saveGacha(key, gacha);
+        this.updateRank(key, res);
+    }
+    getUpPercentage(rankData) {
+        if (rankData.star5 == 0)
+            return 0;
+        return rankData.up / rankData.star5;
+    }
+    getAverageUpCount(rankData) {
+        if (rankData.star5 == 0)
+            return 180;
+        return rankData.total / rankData.star5;
+    }
+    async showrank() {
+        let msg = [" 抽卡统计: "];
+        this.initRank(this.e.user_id, this.e.sender.nickname);
+        const me = rank[this.e.user_id];
+        msg.push(`- ${me.name}: 共 ${me.total} 抽，${me.star5} 个 5* (${me.up} 个 UP`);
+        msg.push("");
+        msg.push("排行榜 (小保底不歪): ");
+        let top1 = Object.values(rank)
+            .filter(r => r.star5 > 0).sort((a, b) => this.getUpPercentage(b) - this.getUpPercentage(a));
+        let top2 = Object.values(rank)
+            .filter(r => r.star5 > 0).sort((a, b) => this.getAverageUpCount(a) - this.getAverageUpCount(b));
+        // show top 5
+        for (let i = 0; i < 5; ++i) {
+            if (i < top1.length) {
+                let r = top1[i];
+                msg.push(`- ${r.name}: ${r.star5} 个 5*，${r.up} 个 UP，小保底不歪 ${this.getUpPercentage(r).toFixed(2)}%`);
+            }
+        }
+        // show top2 5
+        msg.push("");
+        msg.push("排行榜 (平均 5* 抽数): ");
+        for (let i = 0; i < 5; ++i) {
+            if (i < top2.length) {
+                let r = top2[i];
+                msg.push(`- ${r.name}: 共 ${r.total} 抽，${r.star5} 个 5*，平均 ${this.getAverageUpCount(r).toFixed(2)} 抽`);
+            }
+        }
+        // reply msg
+        await this.reply(msg.join('\n'), true);
     }
 }
 ;
